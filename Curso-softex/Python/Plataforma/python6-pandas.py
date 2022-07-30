8319de6c21f6c66987398adb6a351923cf3a4b5e
Lucas Vinicius <92001463+lucasviniciuscode@users.noreply.github.com>
2022-07-30 11:09:18 -0300

    Update python6-pandas.py

--- Curso-softex/Python/Plataforma/python6-pandas.py
+++ Curso-softex/Python/Plataforma/python6-pandas.py
@@ -7,5 +7,11 @@ df["media"] = (df["nota1"] + df["nota2"]) / 2
 df.loc[df["media"] >= 7, "situacao"] = "APROVADO"
 df.loc[df["faltas"] > 5, "situacao"] = "REPROVADO"
 df.loc[df["media"] < 7, "situacao"] = "REPROVADO"
-print(df)
+faltas = df["faltas"].max()
+media = df["media"].median()
+mediaMax = df["media"].max()
+print('maior numero de faltas: ' + str(faltas))
+print('media geral: ' + str(media))
+print('Maior média: ' + str(mediaMax))
+
 df.to_csv("alunos_situacao.csv")
